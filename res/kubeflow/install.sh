b5ca8ada40d1e02d7480749739d22b49ceef22f3
lantgabor <lant.gabor98@gmail.com>
2023-04-23 00:38:48 +0200

    rename components

--- res/kubeflow/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-# Kustomize 5.0 has been fixed by #2399 Mar 13, 2023
-
-while ! kustomize build example | awk '!/well-defined/' | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
